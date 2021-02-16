bfd3bb3cc4e2ed59a8af3aa98bf0bdf0ce87edb6
Daute Rodríguez Rodríguez <dauterguezrguez@gmail.com>
2021-02-16 18:32:07 +0000

    Add: mock service worker setup

---
+++ src/mocks/handlers.ts
@@ -0,0 +1,19 @@
+import { rest } from 'msw';
+import { RequestHandlersList } from 'msw/lib/types/setupWorker/glossary';
+
+export const handlers: RequestHandlersList = [
+	rest.get('https://localhost:3030/scoops', (req, res, ctx) => {
+		return res(
+			ctx.json([
+				{
+					name: 'Chocolate',
+					imagePath: '/images/chocolate.png',
+				},
+				{
+					name: 'Vanilla',
+					imagePath: '/images/vanilla.png',
+				},
+			])
+		);
+	}),
+];
